420ef0cb46f310bdf5d48e013d5c610bb6c846a8
bdgillis <gillisbriand@gmail.com>
2026-09-19 17:09:09 -0500

    add etsy link new tab

--- BRIANGILLISJS.js
+++ BRIANGILLISJS.js
@@ -53,6 +53,7 @@ function four() {
   }
 }
 
+// Slideshow JS
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -82,4 +83,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+
+
 }
